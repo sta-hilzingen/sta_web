f1a563c1c0dbf7730b94f49b14227ff6b68ec0ba
Daniele Noviello <daniele.noviello@gmail.com>
2026-05-08 09:03:32 +0200

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "sta_web",
+
+    'summary': "Web module extention for STA",
+
+    'description': """ """,
+
+    'author': "Daniele Noviello",
+    'website': "https://sta-separator.de/",
+
+    'category': 'Web',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+#    'demo': ['demo/maintenance_demo_sta.xml',],
+#    'assets': {
+#        'web.assets_backend': [
+#            'sta_maintenance/static/src/js/dynamic_notebook_page.js',
+#    ],
+#},
+    'license': 'LGPL-3',
+}
+
